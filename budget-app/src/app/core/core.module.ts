68065cbb9387b7834f537e66b622e4e60001e368
Boris Novikov <b.novikov@elonsoft.ru>
2024-02-18 18:40:37 +0400

    feat: add eslint airbnb

--- budget-app/src/app/core/core.module.ts
+++ budget-app/src/app/core/core.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { ShellComponent } from './shell/shell.component';
 import { HeaderComponent } from './header/header.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { RouterModule } from '@angular/router';
-
-
 
 @NgModule({
   declarations: [
