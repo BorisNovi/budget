0de16a2d4684cb8e1c9c79beb326387179ff5c87
Boris Novikov <b.novikov@elonsoft.ru>
2024-03-23 20:37:28 +0400

    feat: add chart with mocked data

--- budget-app/src/app/page-analytics/analytics/analytics.component.ts
+++ budget-app/src/app/page-analytics/analytics/analytics.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { LocalService } from 'src/app/core/services/local.service';
+import { tuiSum } from '@taiga-ui/cdk';
+import { ExpencesCategories } from 'src/app/core/enums/categories.enum';
 import { ExpenceType } from '../../core/models/add.model';
 
 @Component({
   selector: 'app-analytics',
   templateUrl: './analytics.component.html',
-  styleUrls: ['./analytics.component.scss']
+  styleUrls: ['./analytics.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AnalyticsComponent {
+  readonly labels = Object.values(ExpencesCategories);
   constructor(private localService: LocalService) {
     console.log('expences:', this.localService.get(ExpenceType.expence));
     console.log('incomes:', this.localService.get(ExpenceType.income));
+  }
+
+  activeItemIndex = NaN;
+
+  readonly value = [13769, 12367, 10172, 3018, 2592, 2324, 34343, 34324, 3343];
+  readonly sum = tuiSum(...this.value);
+
+  isItemActive(index: number): boolean {
+    return this.activeItemIndex === index;
+  }
+
+  onHover(index: number, hovered: boolean): void {
+    this.activeItemIndex = hovered ? index : 0;
+  }
+
+  getColor(index: number): string {
+    return `var(--tui-chart-${index})`;
   }
 }
--- budget-app/src/app/page-analytics/page-analytics.module.ts
+++ budget-app/src/app/page-analytics/page-analytics.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { TuiIslandModule } from '@taiga-ui/kit';
+import { TuiArcChartModule, TuiBarModule, TuiLegendItemModule, TuiRingChartModule } from '@taiga-ui/addon-charts';
 import { AnalyticsComponent } from './analytics/analytics.component';
+import { TuiMoneyModule } from '@taiga-ui/addon-commerce';
+import { TuiHoveredModule } from '@taiga-ui/cdk';
 
 const routes = [
   { path: '', component: AnalyticsComponent },
@@ -12,6 +16,12 @@ const routes = [
   ],
   imports: [
     CommonModule,
+    TuiIslandModule,
+    TuiLegendItemModule,
+    TuiBarModule,
+    TuiMoneyModule,
+    TuiRingChartModule,
+    TuiHoveredModule,
     RouterModule.forChild(routes),
   ]
 })
