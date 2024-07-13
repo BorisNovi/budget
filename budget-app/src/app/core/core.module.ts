4196fb96f4891b0c56a022476e5ec19b98699581
Boris Novikov <borisnovikov4815@gmail.com>
2024-07-13 16:16:40 +0400

    feat: change layout for add-component and shell-component

--- budget-app/src/app/core/core.module.ts
+++ budget-app/src/app/core/core.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { TuiButtonModule } from '@taiga-ui/core';
+import { TuiButtonModule, TuiGroupModule } from '@taiga-ui/core';
 import { ShellComponent } from './shell/shell.component';
 import { HeaderComponent } from './header/header.component';
 import { NotFoundComponent } from './not-found/not-found.component';
@@ -16,6 +16,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     CommonModule,
     RouterModule,
     TuiButtonModule,
+    TuiGroupModule,
   ]
 })
 export class CoreModule { }
