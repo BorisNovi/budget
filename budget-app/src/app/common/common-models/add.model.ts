4ba2765fd7035026678baef19fed4475359f850b
Boris Novikov <borisnovikov4815@gmail.com>
2024-08-05 00:18:16 +0400

    feat: change input mode

--- budget-app/src/app/common/common-models/add.model.ts
+++ budget-app/src/app/common/common-models/add.model.ts
@@ -2,6 +2,7 @@ import { TuiDay } from '@taiga-ui/cdk';
 import { ExpenceTypeKey } from '../common-enums/expenceTypeKey.enum';
 
 export interface IAdd {
+  id?: number; // TODO: Как ты мог вообще забыть про ID?
   amount: number;
   expencesType: ExpenceTypeKey;
   date: TuiDay;
